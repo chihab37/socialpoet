1f70497ab6bf0c8470602466fe89381c52e71c38
chihab37 <125484590+chihab37@users.noreply.github.com>
2024-05-28 19:46:20 +0200

    Update vue.config.js

--- web_client/vue.config.js
+++ web_client/vue.config.js
@@ -1,12 +1,12 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
   chainWebpack(config) {
-    // Only convert .svg files that are imported by these files as Vue component
     const FILE_RE = /\.(vue|js|ts|svg)$/;
 
-    // Use vue-cli's default rule for svg in non .vue .js .ts files
     config.module.rule('svg').issuer((file) => !FILE_RE.test(file));
 
-    // Use our loader to handle svg imported by other files
     config.module
       .rule('svg-component')
       .test(/\.svg$/)
